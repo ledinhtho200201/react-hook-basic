747a712c8ff347ff71a644402d19772c554619d4
JRabbit <ledinhtho686@gmail.com>
2023-05-10 21:09:30 +0700

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  let [name, setName] = useState('Thold');
+  let [address, setAddress] = useState('');
+
+  const handleEventClick = (event) => {
+    console.log(address)
+    setName(address)
+  }
+
+  const handleOnChangeInput = (event) => {
+    setAddress(event.target.value)
+  }
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Todos app with reactHook</h1>
+        <h1>Todos app with {name} !</h1>
+        <input type='text' value={address} onChange={(event) => handleOnChangeInput(event)} />
+        <button type='button' onClick={(event) => { handleEventClick(event) }}>Click me!</button>
       </header>
     </div>
   );
