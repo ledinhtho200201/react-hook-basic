eb43152ea2f3ac628d8b8a0eab73e2ee40cbc9b2
JRabbit <ledinhtho686@gmail.com>
2023-05-12 00:32:39 +0700

    outputting lists

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import { useState } from 'react';
 const App = () => {
   let [name, setName] = useState('Thold');
   let [address, setAddress] = useState('');
+  const [todos, setTodos] = useState([
+    { id: 'todo1', title: 'watching video' },
+    { id: 'todo2', title: 'reading book' },
+    { id: 'todo3', title: 'playing game' },
+  ]);
 
   const handleEventClick = (event) => {
-    console.log(address)
-    setName(address)
+    if (!address) {
+      alert('empty input');
+      return;
+    }
+    let newTodo = { id: 'abc', title: address }
+    setTodos([...todos, newTodo])
+    setAddress('')
+
   }
 
   const handleOnChangeInput = (event) => {
@@ -22,6 +33,13 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Todos app with {name} !</h1>
+        <div className='todos-container'>
+          {todos.map(todo => {
+            return (
+              <li className='todo-child' key={todo.id}>{todo.title}</li>
+            )
+          })}
+        </div>
         <input type='text' value={address} onChange={(event) => handleOnChangeInput(event)} />
         <button type='button' onClick={(event) => { handleEventClick(event) }}>Click me!</button>
       </header>
