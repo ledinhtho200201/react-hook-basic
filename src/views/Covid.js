bc88de7668d9bdeedf9b5f33ddb90ff64d5b67b8
JRabbit <ledinhtho686@gmail.com>
2023-05-15 14:02:37 +0700

    conditional loading data

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -3,13 +3,18 @@ import axios from 'axios';
 
 const Covid = () => {
     const [dataFood, setDataFood] = useState([]);
-
+    const [loading, setLoading] = useState(true);
 
     //componentDidMount
     useEffect(async () => {
-        let res = await axios.get('https://private-anon-7bc3d81422-pizzaapp.apiary-mock.com/restaurants/restaurantId/menu?category=Pizza&orderBy=rank')
-        let data = res && res.data ? res.data : [];
-        setDataFood(data);
+        setTimeout(async () => {
+            let res = await axios.get('https://private-anon-7bc3d81422-pizzaapp.apiary-mock.com/restaurants/restaurantId/menu?category=Pizza&orderBy=rank')
+            let data = res && res.data ? res.data : [];
+            setDataFood(data);
+            setLoading(false);
+        }, 3000)
+
+
     }, []);
 
     return (
@@ -24,7 +29,7 @@ const Covid = () => {
                 </tr>
             </thead>
             <tbody>
-                {dataFood && dataFood.length > 0 && dataFood.map(item => {
+                {!loading && dataFood && dataFood.length > 0 && dataFood.map(item => {
                     return (
                         <tr key={item.id}>
                             <td>{item.category}</td>
@@ -34,6 +39,12 @@ const Covid = () => {
                         </tr>
                     )
                 })}
+
+                {loading &&
+                    <tr>
+                        <td colSpan='5' style={{ 'textAlign': 'center' }}>Loading ... </td>
+                    </tr>
+                }
             </tbody>
         </table>
     )
