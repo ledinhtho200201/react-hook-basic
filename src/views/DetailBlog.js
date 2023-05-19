04a5ed9ba9ef10ce36a029f3fb0c29d4869e69c2
JRabbit <ledinhtho686@gmail.com>
2023-05-19 17:23:53 +0700

    reuse custom hook

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,17 +1,32 @@
 import { useParams, useHistory } from "react-router-dom";
+import useFetch from "../customize/fetch";
+import './Blog.scss';
 
 const DetailBlog = () => {
     let { id } = useParams();
     let history = useHistory();
 
+    const { data: dataBlogsDetail, isLoading, isError } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+
     const handleBackData = () => {
         history.push("/blog");
     }
-
+    console.log('>>>check data detail: ', dataBlogsDetail)
     return (
         <>
             <div><span onClick={handleBackData}>&lt;-- Back</span></div>
-            <h1>Hello detail blog with id = {id}</h1>
+            <div className="blog-detail">
+                {dataBlogsDetail &&
+                    <>
+                        <div className="title">
+                            Blog Id: {id} --- {isLoading === true ? 'Loading data ...' : dataBlogsDetail.title}
+                        </div>
+                        <div className="content">
+                            {dataBlogsDetail.body}
+                        </div>
+                    </>
+                }
+            </div>
         </>
     )
 }
